e158d32e3cdc6f6bb54b9ef9678ee27c8a3ec941
Robbie08 <rortiz8838@sdsu.edu>
2022-05-12 15:53:47 -0700

    New: initialized this project as a gomod, we have a go mod and sum file now

---
+++ server/go.mod
@@ -0,0 +1,5 @@
+module github.com/Robbie08/SmartSafe2.0
+
+go 1.13
+
+require github.com/sirupsen/logrus v1.8.1 // indirect
--- server/main.go
+++ server/main.go
@@ -2,8 +2,42 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	log "github.com/sirupsen/logrus" // library that helps with loging and monitoring
 )
+
+var PORT string = ":8080"
 
 func main() {
 	fmt.Printf("%s\n", "hello world!")
+	log.SetFormatter(&log.JSONFormatter{})
+	log.Info("Starting Server...")
+	http.HandleFunc("/", defEndPoint)
+	http.HandleFunc("/api/v2/open_safe", openSafeEndPoint)
+	http.HandleFunc("/api/v2/validate", validateUserEndPoint)
+	http.ListenAndServe(PORT, nil)
+}
+
+func defEndPoint(w http.ResponseWriter, req *http.Request) {
+
+}
+
+func openSafeEndPoint(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case "GET":
+		log.Info("Someone hit the openSafe endpoint")
+	default:
+		fmt.Printf("%s\n", "Only accepting GET requests")
+	}
+
+}
+
+func validateUserEndPoint(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case "GET":
+		log.Info("Someone hit the validateEndPoint")
+	default:
+		fmt.Printf("%s\n", "Only accepting GET requests")
+	}
 }
