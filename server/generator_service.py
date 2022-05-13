038b3ee24840eb3f126ea7eb21b6df96ab12b73b
Robbie08 <rortiz8838@sdsu.edu>
2022-05-12 22:23:52 -0700

    New: created code that generates temporary password and sends it via Twilio as an sms

---
+++ server/generator_service.py
@@ -0,0 +1,30 @@
+import requests as req
+import random
+import keys
+from twilio.rest import Client
+
+
+
+# This function will send our payload with password
+# @params: A password that we will send over to our http server
+def sendPayload(passwd):
+    payload = {'message': passwd}
+    req.post('http://localhost:8080/pyclient', params=payload)
+
+# This function will generate a password
+# @params length: determines the number of characters in our password
+# @return : generated password
+def generatePassword(length):
+    return ''.join([chr(random.randint(65,126)) for _ in range(length)])
+password = generatePassword(10)
+
+#make Client to send message
+client = Client(keys.account_sid , keys.auth_token)
+#This sends the message to a given phone number
+message = client.messages.create(
+    body = password,#the password that is generated
+    from_= keys.twilio_number,#twilio phone number
+    to = keys.target_number #This can be any phone number, must in this format +1##########
+)
+print(message.body)
+#sendPayload(password) # Generate password, package it into a payload and send it via http
